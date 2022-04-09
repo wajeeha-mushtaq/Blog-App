ed5f214f6c506792fbbb124898aad90e5bbb96f6
wajeeha-mushtaq <wajeeha.mushtaqq@devsinc.com>
2022-04-09 12:05:14 +0500

    Added Post CRU

--- api/index.js
+++ api/index.js
@@ -4,6 +4,8 @@ const dotenv = require("dotenv");
 const mongoose = require('mongoose');
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+
 
 dotenv.config();
 // send json to body otherwise 500 error
@@ -20,8 +22,10 @@ mongoose.connect(process.env.MONGO_URL, {
 
 // middleware for auth route
 app.use("/api/auth", authRoute);
-
+// middleware for users route
 app.use("/api/users", userRoute);
+// middleware for post route
+app.use("/api/posts", postRoute);
 
 // listen server
 app.listen("5000", ()=>{
