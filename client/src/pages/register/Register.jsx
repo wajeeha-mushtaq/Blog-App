d85c83b4d2504b94bdae2167a46544946f868acf
wajeeha-mushtaq <wajeeha.mushtaqq@devsinc.com>
2022-04-11 21:06:11 +0500

    Added register system

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,19 +1,56 @@
+import { useState } from "react";
+import axios from "axios";
 import "./register.css"
 
 export default function Register() {
-    return (
-        <div className="register">
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError(false);
+    try{
+      const res = await axios.post("/auth/rergister", {
+        username,
+        email,
+        password
+      });
+      res.data && window.replace("/login");
+    } catch(err){
+        setError(true);
+    }
+  }
+  return (
+    <div className="register">
       <span className="registerTitle">Register</span>
-      <form className="registerForm">
+      <form className="registerForm" onSubmit={handleSubmit}>
         <label>Username</label>
-        <input className="registerInput" type="text" placeholder="Enter your username..." />
+        <input
+          className="registerInput"
+          type="text"
+          placeholder="Enter your username..."
+          onChange={e=>setUsername(e.target.value)}
+        />
         <label>Email</label>
-        <input className="registerInput" type="text" placeholder="Enter your email..." />
+        <input
+          className="registerInput"
+          type="text"
+          placeholder="Enter your email..."
+          onChange={e=>setEmail(e.target.value)}
+        />
         <label>Password</label>
-        <input className="registerInput" type="password" placeholder="Enter your password..." />
-        <button className="registerButton">Register</button>
+        <input
+          className="registerInput"
+          type="password"
+          placeholder="Enter your password..."
+          onChange={e=>setPassword(e.target.value)}
+        />
+        <button className="registerButton" type="submit">Register</button>
       </form>
-        <button className="registerLoginButton">Login</button>
+      <button className="registerLoginButton">Login</button>
+     {error &&  <span style={{color: "red", marginTop: "10px"}}>Wrong credentials</span>}
     </div>
-    )
+  )
 }
