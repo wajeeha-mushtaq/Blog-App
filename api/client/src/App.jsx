07e7ad1bf71644da772b7aee86f31475e3c296fe
wajeeha-mushtaq <wajeeha.mushtaqq@devsinc.com>
2022-04-13 13:12:10 +0500

    deployapp

---
+++ api/client/src/App.jsx
@@ -0,0 +1,36 @@
+import Topbar from "./components/topbar/Topbar";
+import Homepage from "./pages/homepage/Homepage";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useContext } from "react";
+import { Context } from "./context/Context";
+
+function App() {
+  const {user} = useContext(Context);
+  return (
+    <Router>
+      <Topbar />
+      <Switch>
+        <Route exact path="/">
+          <Homepage />
+        </Route>
+        <Route path="/posts">
+          <Homepage />
+        </Route>
+        <Route path="/register">
+          {user ? <Homepage /> : <Register />}
+        </Route>
+        <Route path="/login">{user ? <Homepage /> : <Login />}</Route>
+        <Route path="/post/:id">
+          <Single />
+        </Route>
+        <Route path="/write">{user ? <Write /> : <Login />}</Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
---
+++ api/client/src/components/post/Post.jsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+import "./post.css";
+
+export default function Post({post}) {
+  const PF = "http://localhost:5000/images/";
+  return (
+    <div className="post">
+      {post.photo && (
+        <img
+          className="postImg"
+          src={PF + post.photo}
+          alt=""
+        />
+      )}
+      <div className="postInfo">
+        <span className="postTitle">
+          <Link to={`/post/${post._id}`} className="link">
+            {post.title}
+          </Link>
+        </span>
+        <hr />
+        <span className="postDate">{post.createdAt}</span>
+      </div>
+      <p className="postDesc">
+        {post.desc}
+      </p>
+    </div>
+  );
+}
